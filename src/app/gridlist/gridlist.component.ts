77c413d834bb204bb93736d2dad21c51a1ed45c9
Robby Lewis <Robbysim37@gmail.com>
2024-04-22 18:15:26 -0400

    accordions and grids

---
+++ src/app/gridlist/gridlist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gridlist',
+  templateUrl: './gridlist.component.html',
+  styleUrl: './gridlist.component.css'
+})
+export class GridlistComponent {
+
+}
