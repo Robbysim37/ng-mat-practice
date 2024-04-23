9ea696b15b1d42c91a29d294781648cf47064375
Robby Lewis <Robbysim37@gmail.com>
2024-04-23 13:38:34 -0400

    steppers

---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrl: './tabs.component.css'
+})
+export class TabsComponent {
+  tabChange = () => {
+    console.log("tab changed!")
+  }
+}
